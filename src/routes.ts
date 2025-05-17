d9fa6ff1429d82848515c8d4ba6ee4bd8072950c
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-05-17 19:22:56 -0300

    feat: handle errors with express-async-errors

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/teste", (req: Request, res: Response) => {
-  return res.json({ ok: "OK" });
+  throw new Error("erro ao fazer essa requisição");
 });
 
 export { router };
