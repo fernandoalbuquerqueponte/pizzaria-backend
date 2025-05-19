f469690bcee4f79a93b55aca8601d7d55d356606
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-05-19 14:48:44 -0300

    feat: add initial create user service

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,8 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+import { CreateUserController } from "./controllers/user/CreateUserController";
 
 const router = Router();
 
-router.get("/teste", (req: Request, res: Response) => {
-  throw new Error("erro ao fazer essa requisiçãso");
-});
+router.post("/users", new CreateUserController().handle);
 
 export { router };
