f84869215577e3136832ffdb9ddd427ec17ca180
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-05-22 16:36:45 -0300

    feat: add authenticated user details response with middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 
+import { UserDetailsController } from "./controllers/user/UserDetailsController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+
+import { isUserAuthenticated } from "./middlewares/isUserAuthenticated";
 
 const router = Router();
 
 router.post("/users", new CreateUserController().handle);
 
 router.post("/session", new AuthUserController().handle);
+
+router.get("/me", isUserAuthenticated, new UserDetailsController().handle);
 
 export { router };
