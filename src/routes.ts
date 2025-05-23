fb2fe54be648cb274e69b56ad3ef6839d2b9363c
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-05-23 17:08:25 -0300

    feat: create new category

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { UserDetailsController } from "./controllers/user/UserDetailsController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
@@ -8,10 +9,20 @@ import { isUserAuthenticated } from "./middlewares/isUserAuthenticated";
 
 const router = Router();
 
+//-- ROTAS USER --
+
 router.post("/users", new CreateUserController().handle);
 
 router.post("/session", new AuthUserController().handle);
 
 router.get("/me", isUserAuthenticated, new UserDetailsController().handle);
+
+// -- ROTAS CATEGORY --
+
+router.post(
+  "/category",
+  isUserAuthenticated,
+  new CreateCategoryController().handle
+);
 
 export { router };
